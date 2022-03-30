02e24be67ce28d8542488fce73f672fd6e6898e8
Matt Kucia <matt.kucia@motional.com>
2022-03-30 19:29:35 +0800

    features: Add configuration file requirement

--- features/service.feature
+++ features/service.feature
@@ -6,3 +6,14 @@ Scenario: expose /health endpoint
      Then empty body response is returned
      Then HTTP code 200 is returned
 
+
+Scenario Outline: read configuration file
+     When application started
+    Given "../features/example-config.yaml" config file is read
+     Then configuration "<option>" is available for the application under "<name>"
+
+     Examples:
+       | option   | name               |
+       | URL      | http://api.example |
+       | Location | Place              |
+       | LOCatiON | Place              |
--- features/steps/service.py
+++ features/steps/service.py
@@ -24,3 +24,14 @@ def step_then_empty_body(context):
 @then('HTTP code 200 is returned')
 def step_impl(context):
     assert context.response.status_code == 200
+
+
+@given('config file is read')
+def step_conf_load(context):
+    raise NotImplementedError(u'STEP: Given config file is read')
+
+
+@then('configuration "{name}" is available for the application under "{value}"')
+def step_conf(context):
+    raise NotImplementedError(u'STEP: Then configuration "URL" is available '
+                              u'for the application under "http://api.example"')
