17de41f017c7195c5d8cd3127a84973da00fa093
Matt Kucia <matt.kucia@motional.com>
2022-03-31 00:30:23 +0800

    Implement gov rain api client with formatter

--- features/steps/govapiclient.py
+++ features/steps/govapiclient.py
@@ -1,18 +1,26 @@
+from datetime import datetime
+
 from behave import when, then, given
 
-
-@when('requesting rainfall data for "{location}" at timestamp "{timedate}"')
-def step_when_request(context, location, timedate):
-    raise NotImplementedError(u'STEP: When requesting rainfall data '
-                              u'for "Alexandra Road" at timestamp '
-                              u'"2022-01-02T11:00:00"')
+from rainfall.govapi import GovApiClient, FormatterCSV
 
 
-@then(u'the response is "{response}"')
-def step_then_response(context, response):
-    raise NotImplementedError(u'STEP: Then the response is "0.2"')
+@given(u'api client')
+def step_client(context):
+    context.client = GovApiClient()
 
 
 @given(u'data formatter in "CSV" format')
 def step_given_formatter(context):
-    raise NotImplementedError(u'STEP: Given data formatter in "CSV" format')
+    context.client.set_formatter(FormatterCSV)
+
+
+@when('requesting rainfall data for "{location}" at timestamp "{timedate}"')
+def step_when_request(context, location, timedate):
+    timedate = datetime.strptime(timedate, '%Y-%m-%dT%H:%M:%S')
+    context.response = context.client.get(location, timedate)
+
+
+@then(u'the response is "{response}"')
+def step_then_response(context, response):
+    assert context.response == response
--- rainfall/govapi.py
+++ rainfall/govapi.py
@@ -1,6 +1,59 @@
+from datetime import datetime
+
+import requests
+
+
+class FormatterRawValue(object):
+    def format(payload):
+        return str(payload["value"])
+
+
+class FormatterCSV(FormatterRawValue):
+    def format(payload):
+        amount = payload["value"]
+        raining = "Raining" if amount > 0 else "Not Raining"
+        time = payload["time"].strftime("%H:%M")
+        return f'{payload["name"]}, {time}, {amount}mm, {raining}'
+
+
 class GovApiClient(object):
-    def __init__(self) -> None:
-        self.url = "https://api.data.gov.sg/v1/environment/rainfall"
+    def __init__(self, url=None, formatter=None):
+        self.url = url or "https://api.data.gov.sg/v1/environment/rainfall"
+        self.formatter = formatter or FormatterRawValue
 
     def get(self, location, timestamp=None):
-        pass
+        if timestamp:
+            params = {"date_time": timestamp.strftime("%Y-%m-%dT%H:%M:%S")}
+        else:
+            params = None
+        response = requests.get(self.url, params)
+        response.raise_for_status()
+        data = response.json()
+        station_id = next(
+            (
+                station["id"]
+                for station in data.get("metadata", {}).get("stations", [])
+                if station["name"] == location
+            ),
+            None,
+        )
+        if station_id is None:
+            return None
+        payload = {
+            "name": location,
+            "time": datetime.strptime(
+                data["items"][0]["timestamp"], "%Y-%m-%dT%H:%M:%S%z"
+            ),
+            "value": next(
+                (
+                    r["value"]
+                    for r in data["items"][0]["readings"]
+                    if r["station_id"] == station_id
+                ),
+                None,
+            ),
+        }
+        return self.formatter.format(payload)
+
+    def set_formatter(self, formatter):
+        self.formatter = formatter
